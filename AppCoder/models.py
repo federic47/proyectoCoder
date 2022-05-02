7aacc466ba1c001e5c39c601a1be3cd014ec33f0
Fede Koss <federic47@hotmail.com>
2022-05-02 20:21:59 -0300

    tercer  commit hasta clase 20  todo ok!

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,10 +4,16 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
 
+    def __str__(self):
+        return self.nombre+" "+ str(self.comision)
+
 class Estudiante(models.Model):
     nombre= models.CharField(max_length=50)
     apellido= models.CharField(max_length=50)
     email= models.EmailField()
+
+    def __str__(self):
+        return self.nombre+" "+ self.apellido
 
 class Profesor(models.Model):
     nombre= models.CharField(max_length=50)
@@ -19,4 +25,9 @@ class Entregable(models.Model):
     nombre= models.CharField(max_length=50)
     fecha_entrega= models.DateField()
     entregado= models.BooleanField()
+
+    def __str__(self):
+        return self.nombre+" "+ str(self.fecha_entrega)
+
+
 
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('profesores/', profesores),
-    path('cursos/', cursos),
-    path('entregables/', entregables),
+    path('', inicio, name= 'inicio'),
+    path('profesores/', profesores, name='profesores'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('cursos/', cursos, name='cursos'),
+    path('entregables/', entregables, name='entregables'),
 
 ]
